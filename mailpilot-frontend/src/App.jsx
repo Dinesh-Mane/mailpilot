bb77fc40b978ed967df4e83506bd270c496eea18
Dinesh <dinesh30mane@gmail.com>
2026-09-08 00:42:31 +0530

    add a form in UI which will take email content and tone from user

--- mailpilot-frontend/src/App.jsx
+++ mailpilot-frontend/src/App.jsx
@@ -1,17 +1,79 @@
-import { useState } from 'react'
-import heroImg from './assets/hero.png'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import { Box, Container, Typography, TextField, FormControl, InputLabel, Select, MenuItem, Button, CircularProgress } from '@mui/material';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [emailContent, setEmailContent] = useState('');
+  const [tone, setTone] = useState('');
+  const [generatedReply, setGeneratedReply] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState('');
+
+  const handleSubmit = async () => {
+
+  }
 
   return (
-    <>
-      <p>Hello Dineshh! shall we start???</p>
-    </>
-  )
+    <Container maxWidth="md" sx={{ py: 6 }}>
+
+      <Typography
+        variant="h2"
+        component="h1"
+        fontWeight={700}
+        sx={{ mb: 1 }}
+      >
+        MailPilot
+      </Typography>
+
+      <Typography
+        variant="h6"
+        component="p"
+        color="text.secondary"
+        sx={{
+          fontStyle: 'italic',
+          fontWeight: 400,
+        }}
+      >
+        Your AI copilot for better emails
+      </Typography>
+      <br />
+      <Box sx={{mx:3}}>
+        <TextField
+          fullWidth
+          multiline
+          rows={6}
+          variant='outlined'
+          label="Original Email Content"
+          value={emailContent || ''}
+          onChange={(e) => setEmailContent(e.target.value)}
+          sx={{ mb:2 }}/>
+
+          <FormControl fullWidth>
+            <InputLabel>Tone (Optional)</InputLabel>
+            <Select
+              value={tone || ''}
+              label={"Tone (Optional)"}
+              onChange={(e) => setTone(e.target.value)}
+              sx={{ mb:2 }}>
+                <MenuItem value="">None</MenuItem>
+                <MenuItem value="Professional">Professional</MenuItem>
+                <MenuItem value="Casual">Casual</MenuItem>
+                <MenuItem value="Friendly">Friendly</MenuItem>
+            </Select>
+          </FormControl>
+
+          <Button
+            variant='contained'
+            onClick={handleSubmit}
+            disabled={!emailContent || loading}
+            fullWidth
+            sx={{ mb:2 }}>
+            {loading? <CircularProgress size={2}/> : "Generate Reply"}
+          </Button>
+
+      </Box>
+    </Container>
+  );
 }
 
-export default App
+export default App;
